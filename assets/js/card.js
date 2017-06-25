2849015169a9fb4c2bceea9791aede090e15b459
Florian SAUMUROT <florian.saumurot@gmail.com>
2017-06-25 11:44:34 +0200

    carte in progress

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-
+    if ($(window).width() > 768) {
+        window.onscroll = function () {
+            if ($('body').scrollTop() > 600) {
+                $('#menu-prin').css({
+                    position: 'fixed',
+                    background: 'rgba(0, 0, 0, 0.5)',
+                    transition: 'all ease 1s'
+                });
+                $('nav#menu-prin>div~div>ul li a').css('color', 'white');
+                $('i').css('color', 'white');
+                $('#cont-menu').css('position', 'static');
+            } else {
+                $('#menu-prin').css({
+                    position: 'relative',
+                    background: 'transparent',
+                    transition: 'all ease 1s'
+                });
+                $('nav#menu-prin>div~div>ul li a').css('color', 'black');
+                $('i').css('color', 'black');
+                $('#cont-menu').css('position', 'absolute');
+            }
+        }
+    }
 });
