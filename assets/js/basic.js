0d2be03db3a686901bdef3297ffb1f3434429744
Florian SAUMUROT <florian.saumurot@gmail.com>
2017-06-26 11:34:16 +0200

    carte in progress

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -15,77 +15,79 @@ $(document).ready(function () {
         return false;
     });
 
-    $('#resto').hover(function () {
-        $('#menu-prin').css('height', '100px');
-        $('#resto ul').fadeIn('low');
-        console.log('toto');
-        $('#resto ul').hover(function () {
-            $('#resto ul').fadeIn('low');
-        }, function () {
-            $('#resto ul').fadeOut('low');
-        });
-        $('#resto ul li:first-child').hover(function () {
-            $('#resto ul').css('display', 'block');
-            $('#resto ul li:first-child a').css({
-                'border-bottom': 'solid #40C9C5 2px',
-                width: '100%'
-            });
-        }, function () {
-            $('#resto ul a').css('border-bottom', 'none');
-        });
-        $('#resto ul li:last-child').hover(function () {
-            $('#resto ul li:last-child a').css({
-                'border-bottom': 'solid #40C9C5 2px',
-                width: '100%'
-            });
-        }, function () {
-            $('#resto ul a').css('border-bottom', 'none');
-        });
-    }, function () {
-        $('#resto ul').fadeOut('low');
-        $('#menu-prin').css('height', 'auto');
-    });
 
-    $('#actu').hover(function () {
-        $('#menu-prin').css('height', '100px');
-        $('#actu ul').fadeIn('low');
-        $('#actu ul').hover(function () {
-            $('#actu ul').fadeIn('low');
-        }, function () {
-            $('#actu ul').fadeOut('low');
-        });
-        $('#actu ul li:first-child').hover(function () {
-            $('#actu').css('position', 'relative');
-            $('#actu ul').css({
-                position: 'absolute',
-                width: '100%'
-            });
-            $('#actu ul li:first-child a').css({
-                'border-bottom': 'solid #40C9C5 2px',
-                width: '100%'
-            });
-        }, function () {
-            $('#actu ul a').css('border-bottom', 'none');
-        });
-        $('#actu ul li:last-child').hover(function () {
-            $('#actu').css('position', 'relative');
-            $('#actu ul').css({
-                position: 'absolute',
-                width: '100%'
-            });
-            $('#actu ul li:last-child a').css({
-                'border-bottom': 'solid #40C9C5 2px',
-                width: '100%'
-            });
-        }, function () {
-            $('#actu ul a').css('border-bottom', 'none');
-        });
-    }, function () {
-        $('#actu ul').fadeOut('low');
-        $('#menu-prin').css('height', 'auto');
-    });
 
     if ($(window).width() > 768) {
+        $('#resto').hover(function () {
+            $('#menu-prin').css('height', '100px');
+            $('#resto ul').fadeIn('low');
+            console.log('toto');
+            $('#resto ul').hover(function () {
+                $('#resto ul').fadeIn('low');
+            }, function () {
+                $('#resto ul').fadeOut('low');
+            });
+            $('#resto ul li:first-child').hover(function () {
+                $('#resto ul').css('display', 'block');
+                $('#resto ul li:first-child a').css({
+                    'border-bottom': 'solid #40C9C5 2px',
+                    width: '100%'
+                });
+            }, function () {
+                $('#resto ul a').css('border-bottom', 'none');
+            });
+            $('#resto ul li:last-child').hover(function () {
+                $('#resto ul li:last-child a').css({
+                    'border-bottom': 'solid #40C9C5 2px',
+                    width: '100%'
+                });
+            }, function () {
+                $('#resto ul a').css('border-bottom', 'none');
+            });
+        }, function () {
+            $('#resto ul').fadeOut('low');
+            $('#menu-prin').css('height', 'auto');
+        });
+
+        $('#actu').hover(function () {
+            $('#menu-prin').css('height', '100px');
+            $('#actu ul').fadeIn('low');
+            $('#actu ul').hover(function () {
+                $('#actu ul').fadeIn('low');
+            }, function () {
+                $('#actu ul').fadeOut('low');
+            });
+            $('#actu ul li:first-child').hover(function () {
+                $('#actu').css('position', 'relative');
+                $('#actu ul').css({
+                    position: 'absolute',
+                    width: '100%'
+                });
+                $('#actu ul li:first-child a').css({
+                    'border-bottom': 'solid #40C9C5 2px',
+                    width: '100%'
+                });
+            }, function () {
+                $('#actu ul a').css('border-bottom', 'none');
+            });
+            $('#actu ul li:last-child').hover(function () {
+                $('#actu').css('position', 'relative');
+                $('#actu ul').css({
+                    position: 'absolute',
+                    width: '100%'
+                });
+                $('#actu ul li:last-child a').css({
+                    'border-bottom': 'solid #40C9C5 2px',
+                    width: '100%'
+                });
+            }, function () {
+                $('#actu ul a').css('border-bottom', 'none');
+            });
+        }, function () {
+            $('#actu ul').fadeOut('low');
+            $('#menu-prin').css('height', 'auto');
+        });
+
         window.onscroll = function () {
             if ($('body').scrollTop() > 100) {
                 $('body header:first-child').removeClass('center-col');
