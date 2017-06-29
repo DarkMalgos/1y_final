79c78908e2f0bedfbdcecb8589511e42d06b2242
Florian SAUMUROT <florian.saumurot@gmail.com>
2017-06-29 14:52:02 +0200

    mysql done

--- doc/schema.sql
+++ doc/schema.sql
@@ -5,11 +5,11 @@ CREATE DATABASE Mascotte_db; #creation of database
 USE Mascotte_db; #go on database
 
 #create table name students
-CREATE TABLE students (
+CREATE TABLE reservation (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
     firstname VARCHAR(128), 
     lastname VARCHAR(128),
-    age INT, 
-    class VARCHAR(128), 
-    gender VARCHAR(255)
-); 
+    jour DATE NOT NULL, 
+    heure TIME NOT NULL, 
+    nb_couvert INT NOT NULL
+);
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 /* EXPRESS */
 var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
 var app = express();
 /* CREATION DU SERVER */
 var server = require('http').createServer(app);
@@ -11,6 +13,31 @@ app.use('/css', express.static(__dirname + '/assets/css'));
 app.use('/js', express.static(__dirname + '/assets/js'));
 app.use('/img', express.static(__dirname + '/assets/img'));
 app.use('/fonts', express.static(__dirname + '/assets/fonts'));
+
+var connection = function () {
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'mascotte_db'
+    });
+}
+
+//connection for mac
+/*var connection = function () {
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'mascotte_db',
+        socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+    });
+}*/
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 
 /* road for start page */
 app.get('/', function (req, res) {
@@ -55,6 +82,17 @@ app.get('/en/Actualité', function (req, res) {
 
 app.get('/contact', function (req, res) {
     res.sendFile(__dirname + '/views/fr/contact.html');
+});
+
+app.post('/contact', function (req, res) {
+    var q = "INSERT INTO reservation (firstname, lastname, jour, heure, nb_couvert) VALUES ('" + req.body.firstname + "', '" + req.body.lastname + "', '" + req.body.datepicker + "', '" + req.body.timepicker + "', " + req.body.number + ");",
+        co = connection();
+    co.connect();
+    co.query(q, function (error, results, fields) {
+        if (error) return console.log(error);
+        res.sendFile(__dirname + '/views/fr/contact.html');
+    });
+    co.end();
 });
 
 app.get('/en/contact', function (req, res) {
